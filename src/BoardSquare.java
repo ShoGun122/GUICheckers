0f1cd67ba8e09e53ae59b57dc3f0d589263cd74e
ShoGun122 <kapel125@gmail.com>
2019-05-31 14:30:39 +0200

    Just Board

---
+++ src/BoardSquare.java
@@ -0,0 +1,52 @@
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+public class BoardSquare extends Region {
+    private Color color;
+    private final int ROW_COUNT=10;
+    private final int COL_COUNT=10;
+    public BoardSquare(Color defaultColor) {
+        color = defaultColor;
+        setColor(color);
+        setPrefSize(200, 200);
+    }
+    public void highlight() {
+        setColor(Color.YELLOW);
+    }
+    public void blacken() {
+        setColor(color);
+    }
+    public void setColor(Color color) {
+        BackgroundFill bgFill = new BackgroundFill(color, CornerRadii.EMPTY, new Insets(2));
+        Background bg = new Background(bgFill);
+        setBackground(bg);
+    }
+
+    public GridPane getBoard(){
+        GridPane board=new GridPane();
+        for(int row=0; row<ROW_COUNT;row++){
+            for(int col=0;col<COL_COUNT;col++){
+                board.add(getBoardSquare(),row,col);
+            }
+        }
+        for(int row=0;row<ROW_COUNT;row++){
+            RowConstraints constrains=new RowConstraints();
+            constrains.setPercentHeight(20);;
+            board.getRowConstraints().add(constrains);
+        }
+        for(int col=0;col<COL_COUNT;col++){
+            ColumnConstraints constrains=new ColumnConstraints();
+            constrains.setPercentWidth(20);
+            board.getColumnConstraints().add(constrains);
+        }
+        return board;
+    }
+    public  BoardSquare getBoardSquare(){
+        BoardSquare square=new BoardSquare(Color.BLACK);
+        square.setOnMouseEntered(e -> square.highlight());
+        square.setOnMouseExited(e -> square.blacken());
+        return square;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+import javafx.*;
+import javafx.scene.paint.Color;
+import java.awt.*;
+
+import java.awt.Color.*;
+
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Scene scene = new Scene (getBoard(),200 ,200 ,Color.GRAY);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public GridPane getBoard()
+    {
+
+        GridPane board = new GridPane();
+        //  i = row
+        //  j = column
+
+        for(int row = 0; row <10 ; row++)
+        {
+            if(row%2==0)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    if (col % 2 == 0)
+                    {
+                        BoardSquare square = new BoardSquare(Color.BLACK);
+                        board.add(square,row,col);
+                    } else
+                    {
+                        BoardSquare square = new BoardSquare(Color.WHITE);
+                        board.add(square,row,col);
+                    }
+                }
+            }
+            else
+            {
+                for(int col =0; col<10; col ++)
+                {
+                    if(col%2!=0)
+                    {
+                        BoardSquare square=new BoardSquare(Color.BLACK);
+                        board.add(square,row,col);
+                    }
+                    else
+                    {
+                        BoardSquare square=new BoardSquare(Color.WHITE);
+                        board.add(square,row,col);
+                    }
+                }
+            }
+        }
+        for(int row = 0; row<10; row++)
+        {
+            RowConstraints contstraint = new RowConstraints();
+            contstraint.setPercentHeight(20);
+            board.getRowConstraints().add(contstraint);
+        }
+        for(int col = 0; col<10; col++)
+        {
+            ColumnConstraints colConstraint = new ColumnConstraints();
+            colConstraint.setPercentWidth(20);
+            board.getColumnConstraints().add(colConstraint);
+        }
+        return board;
+    }
+
+}
